71d34e0d5586fbcac29a2072efc2d94605946aa8
Thien Tran <tranthien1882003@gmail.com>
2026-01-20 02:33:50 +0700

    update

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { checkJwt } from '../middlewares/auth.middleware';
 import { attachUserFromJwt } from '../middlewares/user.middleware';
-import { roleMiddleware } from '../middlewares/role.middleware';
 import { AuthController } from '../controllers/auth.controller';
 
 const authController = new AuthController();
